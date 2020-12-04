093b51223512916b2021772c9eb57f30abd2c8d6
Gabriele Canepa <git@gabrielecanepa.com>
2020-12-04 18:13:27 +0100

    Add book model and migration

---
+++ api/migrations/20201204152445-create-book.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      publisher: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async queryInterface => {
+    await queryInterface.dropTable('Books')
+  },
+}

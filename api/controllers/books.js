b8be41b06caa2e86df8c08172ad4b0ac8a769569
Gabriele Canepa <git@gabrielecanepa.com>
2020-12-06 11:28:55 +0100

    Make order_by case insensitive

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -2,7 +2,7 @@ const db = require('../models')
 
 const {
   Book,
-  Sequelize: { Op },
+  Sequelize: { col, fn, Op },
 } = db
 
 const getAllBooks = async (req, res) => {
@@ -16,7 +16,7 @@ const getAllBooks = async (req, res) => {
           publisher: { [Op.iLike]: `%${q}%` },
         },
       },
-      order: ['title', 'author', 'publisher'].includes(order_by) && [[order_by, 'ASC']],
+      order: ['title', 'author', 'publisher'].includes(order_by) && [[fn('lower', col(order_by)), 'ASC']],
     })
     return res.status(200).json(books)
   } catch (error) {

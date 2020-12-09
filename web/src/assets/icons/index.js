740d86ff8b59c2a342a071e35324221700394e11
Gabriele Canepa <git@gabrielecanepa.com>
2020-12-09 12:27:30 +0100

    Remove unused icon

--- web/src/assets/icons/index.js
+++ web/src/assets/icons/index.js
@@ -1,6 +1,5 @@
-import { ReactComponent as Close } from './close.svg'
 import { ReactComponent as Error } from './error.svg'
 import { ReactComponent as Loader } from './loader.svg'
 import { ReactComponent as Magnifier } from './magnifier.svg'
 
-export { Close, Error, Loader, Magnifier }
+export { Error, Loader, Magnifier }
